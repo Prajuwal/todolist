2bfabb12a449b05b5622ad044f094b8120619b20
Prajuwal <prajuwalsingh@gmail.com>
2022-11-13 17:47:41 +0530

    first commit and completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [currentTask,setCurrentTask] = useState("")
+const [todoList,setTodoList] = useState([])
+const inputTask = useRef(null)
+const addTask = ()=>{
+  setTodoList([...todoList,{task:currentTask,completed:false}])
+  inputTask.current.value = ""
+  setCurrentTask=""
+}
+const taskToDelete =(taskvalue)=>{
+setTodoList(
+  todoList.filter((taskfordeletion)=>{
+    return taskfordeletion.task!=taskvalue
+  })
+)
+}
+const taskComplete =(taskToComplete)=>{
+setTodoList(
+  todoList.map((task)=>{
+    return taskToComplete ==task.task?{task:taskToComplete,completed:true}:{task:task.task,completed:task.completed?true:false}
+  })
+)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>To Do List</h1>
+     <div>
+     <input ref={inputTask} type='text' placeholder='Task...' 
+     onKeyDown={(event)=>{
+      if(event.keyCode==13) addTask()
+     }}
+     onChange={(event)=>{
+      setCurrentTask(event.target.value)
+     }}></input>
+     <button onClick={addTask}> Add Task </button>
+    </div>
+    <hr/>
+    <ul>
+    {todoList.map((val,key)=>{
+      return (
+        <div id="delandcompTask">
+        <li key={key}>{val.task}</li>
+        <button onClick={()=> taskComplete(val.task)}>Completed</button>
+        <button onClick={()=> taskToDelete(val.task)}>X</button>
+        {val.completed?<h1>task completed</h1>:<h1>task not completed</h1>}
+        </div>
+      )
+    })}
+    </ul>
     </div>
   );
 }
